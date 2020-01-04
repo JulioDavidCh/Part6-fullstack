489f20631043da689f23d191b2fa5eb0210d6c80
Julio <juliodavidchopite@gmail.com>
2020-01-04 02:55:26 -0300

    refactored our app to use our new component to display the list of anecdotes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React from 'react';
-import {voteAction} from './reducers/anecdoteReducer'
+import React from 'react'
 import AnecdoteForm from './components/forms/AnecdoteForm'
+import AnecdoteList from './components/forms/AnecdoteList'
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
   const store = props.store
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
-
-  const vote = (id) => {
-    const newDispatch = voteAction(id)
-    store.dispatch(newDispatch)
-  }
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
+      <AnecdoteList store={store} />
       <AnecdoteForm store={store} />
     </div>
   )
