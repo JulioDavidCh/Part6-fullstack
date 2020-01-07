a2b0b22a58ccccd27abb6973fdfe4099be067d4c
Julio <juliodavidchopite@gmail.com>
2020-01-07 10:40:03 -0300

    removed the initial state of the reducer and added another action creator to initialize the state with its case handler in t he reducer

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,9 +8,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE' :
       const id = action.data.id
@@ -36,6 +25,8 @@ const anecdoteReducer = (state = initialState, action) => {
       const newAnecdote = asObject(action.data.content)
       const newStateWithAnecdote = state.concat(newAnecdote)
       return newStateWithAnecdote
+    case 'INIT_ANECDOTES':
+      return action.data
     default:
       return state
   }
@@ -59,4 +50,11 @@ const addAnecdoteAction = (content) => {
   }
 }
 
-export {anecdoteReducer, voteAction, addAnecdoteAction}
+const initializeAnecdotes = data => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data
+  }
+}
+
+export { anecdoteReducer, voteAction, addAnecdoteAction, initializeAnecdotes }
