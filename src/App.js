2afc5ec25bd6d0628a28b64291841d5864091524
Julio <juliodavidchopite@gmail.com>
2020-01-07 10:40:56 -0300

    initialized the state of the anecdotes by fetching them from the json database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/forms/AnecdoteForm'
 import AnecdoteList from './components/forms/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdoteServices from './services/anecdotes'
+import { connect } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = (props) => {
+
+  useEffect(()=> {
+    anecdoteServices.getAll()
+    .then(anecdotes => {
+      props.initializeAnecdotes(anecdotes)
+    })
+  },[])
 
   return (
     <div>
@@ -16,4 +26,5 @@ const App = () => {
   )
 }
 
-export default App
+
+export default connect(null, { initializeAnecdotes })(App)
