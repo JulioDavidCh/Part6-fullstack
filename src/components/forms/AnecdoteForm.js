d8a8401ff494c31e106aeb419b8b591f5117970d
Julio <juliodavidchopite@gmail.com>
2020-01-07 11:20:25 -0300

    expanded our component to allow anecdotes to be stored in the backend and use the response from the server to update the state of the anecdote list

--- src/components/forms/AnecdoteForm.js
+++ src/components/forms/AnecdoteForm.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {addAnecdoteAction} from '../../reducers/anecdoteReducer'
 import {notificationAction} from '../../reducers/notificationReducer'
 import { connect } from 'react-redux'
+import anecdoteServices from '../../services/anecdotes'
 
 const AnecdoteForm = (props) => {
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const inputContent = event.target.note.value
-    props.addAnecdoteAction(inputContent)
+    const anecdoteToAdd = await anecdoteServices.addNew(inputContent)
+    props.addAnecdoteAction(anecdoteToAdd)
     props.notificationAction(`added ${inputContent}`)
     setTimeout(()=> props.notificationAction(''), 5000)
   }
