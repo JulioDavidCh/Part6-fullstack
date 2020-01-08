8bfa963fb9df8b5b0750f819973dec3945927885
Julio <juliodavidchopite@gmail.com>
2020-01-08 19:55:49 -0300

    made our component send the request to the database with out new action creator

--- src/components/forms/AnecdoteForm.js
+++ src/components/forms/AnecdoteForm.js
@@ -2,15 +2,13 @@ import React from 'react'
 import {addAnecdoteAction} from '../../reducers/anecdoteReducer'
 import {notificationAction} from '../../reducers/notificationReducer'
 import { connect } from 'react-redux'
-import anecdoteServices from '../../services/anecdotes'
 
 const AnecdoteForm = (props) => {
 
   const addAnecdote = async (event) => {
     event.preventDefault()
     const inputContent = event.target.note.value
-    const anecdoteToAdd = await anecdoteServices.addNew(inputContent)
-    props.addAnecdoteAction(anecdoteToAdd)
+    props.addAnecdoteAction(inputContent)
     props.notificationAction(`added ${inputContent}`)
     setTimeout(()=> props.notificationAction(''), 5000)
   }
